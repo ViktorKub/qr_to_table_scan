b5effb1e1e07ed1553c76cb9ae5c978200cf6fc6
ViktorKub <136728619+ViktorKub@users.noreply.github.com>
2026-02-10 13:28:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.0.9'; 
+const CACHE_VERSION = 'v1.0.10'; 
 const CACHE_NAME = `qr-tsd-${CACHE_VERSION}`;
 
 const ASSETS = [
@@ -24,5 +24,6 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
 
 
